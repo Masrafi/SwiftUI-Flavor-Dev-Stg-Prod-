f13125ea347055195b175f06d8d25a4d324e9465
Masrafi <masrafianam@gmail.com>
2024-09-07 21:36:00 +0600

    Initial Commit

---
+++ Flavor/FlavorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorApp.swift
+//  Flavor
+//
+//  Created by Md Khorshed Alam on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FlavorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
